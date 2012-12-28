06fc6a9af1c861f36f901ed869ecc49ebe38b05d
Abizer Nasir <abizern@abizern.org>
2012-12-28 16:55:56 +0000

    Move a pair of braces on their own line up

--- lib/fixbraces.rb
+++ lib/fixbraces.rb
@@ -3,6 +3,10 @@ require "fixbraces/version"
 module Fixbraces
   def Fixbraces.fixbraces(text)
     # Move the opening brace to the same line as the opening clause
-    text.gsub(/\n[ \t]*\{[ \t]*$/, " {")
+    partial_result = text.gsub(/\n[ \t]*\{[ \t]*$/, " {")
+
+    # If there are a pair of braces on their own line move them both up to the
+    # same line as the opening line
+    partial_result.gsub(/\n[ \t]*\{\}[ \t]*$/, " {}")
   end
 end
--- spec/fixbraces_spec.rb
+++ spec/fixbraces_spec.rb
@@ -3,8 +3,15 @@ require 'fixbraces'
 describe Fixbraces do
   let(:split_braces) { "- (void)someMethod\n{  \n    return;\n}" }
   let(:corrected_split_braces) { "- (void)someMethod {\n    return;\n}" }
+  let(:paired_braces) { "@interface SomeClass ()\n{}  \n@end" }
+  let(:corrected_paired_braces) { "@interface SomeClass () {}\n@end" }
 
   it "correctly puts the opening brace on the same line" do
     expect(Fixbraces.fixbraces split_braces).to eq corrected_split_braces
   end
+
+  it "correctly puts paired empty braces on the opening line" do
+    expect(Fixbraces.fixbraces paired_braces).to eq corrected_paired_braces
+  end
+
 end
